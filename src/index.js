0d36cf23ee642ebaac944b339c69aea2651ee3f8
Dzmitry Babak <dzmitry.babak@gmail.com>
2018-09-20 19:41:51 +0300

    adv-zeros/impl-func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let advZero = Number.MAX_VALUE;
+    let xbase = base;
+
+    for (let i = 2; i <= base; i++) {
+        if (xbase % i === 0) {
+            let p = 0;
+            while (xbase % i === 0) {
+                xbase = xbase / i;
+                p++;
+            }
+            let count = 0;
+
+
+            let y = Math.floor(number / i);
+
+
+            while (y > 0) {
+                count += y;
+                y = Math.floor(y / i);
+            }
+            advZero = Math.min(advZero, Math.floor(count / p))
+        }
+    }
+    return advZero;
+
+};
